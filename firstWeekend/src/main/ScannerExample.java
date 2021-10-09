44be15a16c5cb8c0cd5df8a171f48bf826c2e93a
jacekbosiacki <jacek.bosiacki@ocado.com>
2021-10-09 09:25:05 +0200

    Added scanner example to repo

---
+++ firstWeekend/src/main/ScannerExample.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class ScannerExample {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String checkTextLength = scanner.nextLine();
+        System.out.println("Length of that string is: " + checkTextLength.length());
+    }
+}
