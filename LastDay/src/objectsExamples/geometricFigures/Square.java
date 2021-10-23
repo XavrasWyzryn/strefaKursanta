126e98bd8f08e3a2a48e078ccbf39ad06614da8e
jacekbosiacki <jacek.bosiacki@ocado.com>
2021-10-23 15:29:26 +0200

    another Scanner example (more complex)

---
+++ LastDay/src/objectsExamples/geometricFigures/Square.java
@@ -0,0 +1,4 @@
+package objectsExamples.geometricFigures;
+
+public class Square {
+}
--- LastDay/src/palindromeChecking/PalindromeChecker.java
+++ LastDay/src/palindromeChecking/PalindromeChecker.java
@@ -2,6 +2,22 @@ package palindromeChecking;
 
 public class PalindromeChecker {
     public static boolean isPalindrome(int number) {
+        int numberOfDigits = 0;
+        int divideBy = 1;
+        while(number / divideBy > 0) {
+            numberOfDigits++;
+            divideBy *= 10;
+        }
+        int[] digitsArray = new int[numberOfDigits];
+        for (int i = 0; i < numberOfDigits; i++) {
+            digitsArray[i] = number % 10;
+            number /= 10;
+        }
+        for (int i = 0; i < numberOfDigits / 2; i++) {
+            if (digitsArray[i] != digitsArray[numberOfDigits-1-i]) {
+                return false;
+            }
+        }
         return true;
     }
 
